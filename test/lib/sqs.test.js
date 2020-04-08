f886acc48e5e3a49ccb28176751da601267b87c2
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-04-07 20:06:30 -0700

    Check if queue is non-empty; throw errors

--- test/lib/sqs.test.js
+++ test/lib/sqs.test.js
@@ -2,25 +2,46 @@ const test = require('ava')
 const sinon = require('sinon')
 const Sqs = require('../../lib/sqs')
 
-test('sendMessage', async (t) => {
-  const sqs = new Sqs({
+test.beforeEach((t) => {
+  t.context.sqs = new Sqs({
     sqs: {
       sendMessage: sinon.stub().returns({
         promise: async () => (Promise.resolve('fishing poll'))
+      }),
+      getQueueAttributes: sinon.stub().returns({
+        promise: sinon.stub().resolves({
+          Attributes: {
+            ApproximateNumberOfMessages: 1,
+            ApproximateNumberOfMessagesDelayed: 2,
+            ApproximateNumberOfMessagesNotVisible: 3
+          }
+        })
       })
     },
     config: {
-      getQueueUrl: () => 'bandit url',
+      getQueueUrl: () => 'bandit url'
     }
   })
+})
 
+test('sendMessage', async (t) => {
   const payload = {
     ship: 'yard'
   }
-  const res = await sqs.sendMessage(payload)
-  t.true(sqs.sqs.sendMessage.calledWith({ 
-    QueueUrl: 'bandit url', 
-    MessageBody: JSON.stringify(payload) 
+  const res = await t.context.sqs.sendMessage(payload)
+  t.true(t.context.sqs.sqs.sendMessage.calledWith({
+    QueueUrl: 'bandit url',
+    MessageBody: JSON.stringify(payload)
   }))
   t.deepEqual(res, 'fishing poll')
+})
+
+test('getMessageCount | success', async (t) => {
+  t.is(await t.context.sqs.getMessageCount(), 6)
+  t.true(t.context.sqs.sqs.getQueueAttributes.called)
+})
+
+test('getMessageCount | failure', async (t) => {
+  t.context.sqs.sqs.getQueueAttributes().promise.resolves({})
+  await t.throwsAsync(() => t.context.sqs.getMessageCount())
 })

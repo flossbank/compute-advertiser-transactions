f17a61e2ce698267614e305785c3d0181104adef
Joel Wasserman <joel.wasserman8@gmail.com>
2020-04-20 09:49:25 -0700

    ensure that when computing advertiser amount owed, we send the milicents into the queue

--- test/lib/process.test.js
+++ test/lib/process.test.js
@@ -21,14 +21,14 @@ test('computes advertisers balances', async (t) => {
     { _id: 'a2', amountToBill: 10, customerId: 'gon' }, // 10mc
     { _id: 'a3', amountToBill: 1000000, customerId: 'papi' }, // $10
     { _id: 'a4', amountToBill: 500000, customerId: 'john' }, // $5
-    { _id: 'a5', amountToBill: 1001, customerId: 'milroy' }, // 1001mc
+    { _id: 'a5', amountToBill: 25001, customerId: 'milroy' }, // 1001mc
     { _id: 'a6', amountToBill: 10, customerId: 'platwick' } // 10 mc
   ]
 
   const expectedBilledAdvertisers = new Map()
   expectedBilledAdvertisers.set('a3', 1000000)
   expectedBilledAdvertisers.set('a4', 500000)
-  expectedBilledAdvertisers.set('a5', 1000)
+  expectedBilledAdvertisers.set('a5', 25000)
 
   t.context.db.getOwingAdvertisers.resolves(advertisers)
 
@@ -39,6 +39,7 @@ test('computes advertisers balances', async (t) => {
     limiter: t.context.limiter
   })
   t.true(t.context.db.getOwingAdvertisers.calledOnce)
+  t.deepEqual(t.context.sqs.sendMessage.lastCall.args[0].amount, 25000)
   t.deepEqual(result.advertisersBilled, expectedBilledAdvertisers)
 })
 
